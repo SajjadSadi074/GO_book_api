8d0bf12324da9a303a571ddd4971e739c98b5cde
Sajjad <sajjadsadi2203@gmail.com>
2025-05-16 10:42:40 +0600

    Cli complete so far. Faltly added another project inside this one, was removed

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,11 +1,12 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"bookapi/routes"
 	"fmt"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -14,27 +15,19 @@ import (
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `A longer description that spans multiple lines and likely contains examples`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("serve called")
+		fmt.Println("🚀 Starting server on http://localhost:8080...")
+
+		// Use your actual Routes() function to get the router
+		r := routes.Routes()
+
+		if err := http.ListenAndServe(":8080", r); err != nil {
+			fmt.Printf("❌ Server failed: %s\n", err)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(serveCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
